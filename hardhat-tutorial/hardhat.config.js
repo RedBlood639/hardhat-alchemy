f714be9cf18ad8144dc8195fe7ee18742c1703d9
RedBlood639 <forever78419@gmail.com>
2021-12-13 09:37:28 +0800

    feature: Deploying to a live network

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+const { PRIVATE_KEY, ALCHEMY_HTTP_API } = process.env;
+
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    ropsten: {
+      url: ALCHEMY_HTTP_API,
+      accounts: [`${PRIVATE_KEY}`],
+    },
+  },
 };
