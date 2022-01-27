cf48b0c33282ce27782af4e8e15aac678942fc50
redblood639 <albinorabbithunter@gmail.com>
2022-01-27 23:42:11 +0800

    feature: creating task

--- hardhat-simple/hardhat.config.js
+++ hardhat-simple/hardhat.config.js
@@ -1,57 +1,23 @@
-// require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
 
-// // This is a sample Hardhat task. To learn how to create your own go to
-// // https://hardhat.org/guides/create-task.html
-// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-//   const accounts = await hre.ethers.getSigners();
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (taskArgs) => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
 
-//   for (const account of accounts) {
-//     console.log(account.address);
-//   }
-// });
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
 
-// // You need to export an object to set up your config
-// // Go to https://hardhat.org/config/ to learn more
+task("hello-world", "Prints a hello world message").setAction(
+  async (taskArgs, hre) => {
+    await hre.run("print", { message: "Hello, World!" });
+  }
+);
 
-// /**
-//  * @type import('hardhat/config').HardhatUserConfig
-//  */
-// module.exports = {
-//   defaultNetwork: "localhost",
-//   networks: {
-//     hardhat: {
-//       forking: {
-//         url: "https://eth-ropsten.alchemyapi.io/v2/FlOf6LAzxykDJEYcn-F2qlgkyxHu8JGv",
-//       },
-//     },
-//     localhost: {
-//       url: "http://127.0.0.1:8545",
-//       accounts: [
-//         "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
-//       ],
-//     },
-//   },
-//   solidity: {
-//     version: "0.8.4",
-//     settings: {
-//       optimizer: {
-//         enabled: true,
-//         runs: 200,
-//       },
-//     },
-//   },
-//   paths: {
-//     sources: "./contracts",
-//     tests: "./test",
-//     cache: "./cache",
-//     artifacts: "./artifacts",
-//   },
-//   mocha: {
-//     timeout: 40000,
-//   },
-// };
-require("@nomiclabs/hardhat-truffle5");
-
-module.exports = {
-  solidity: "0.7.3",
-};
+subtask("print", "Prints a message")
+  .addParam("message", "The message to print")
+  .setAction(async (taskArgs) => {
+    console.log(taskArgs.message);
+  });
+module.exports = {};
