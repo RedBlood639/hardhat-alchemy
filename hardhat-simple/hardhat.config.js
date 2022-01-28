9fc57313b89ed4449c760a6c10c43f1e2999dc12
redblood639 <albinorabbithunter@gmail.com>
2022-01-28 14:37:53 +0800

    feature:  Testing contracts

--- hardhat-simple/hardhat.config.js
+++ hardhat-simple/hardhat.config.js
@@ -1,23 +1,5 @@
-require("@nomiclabs/hardhat-web3");
+require("@nomiclabs/hardhat-waffle");
 
-task("balance", "Prints an account's balance")
-  .addParam("account", "The account's address")
-  .setAction(async (taskArgs) => {
-    const account = web3.utils.toChecksumAddress(taskArgs.account);
-    const balance = await web3.eth.getBalance(account);
-
-    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
-  });
-
-task("hello-world", "Prints a hello world message").setAction(
-  async (taskArgs, hre) => {
-    await hre.run("print", { message: "Hello, World!" });
-  }
-);
-
-subtask("print", "Prints a message")
-  .addParam("message", "The message to print")
-  .setAction(async (taskArgs) => {
-    console.log(taskArgs.message);
-  });
-module.exports = {};
+module.exports = {
+  solidity: "0.7.3",
+};
