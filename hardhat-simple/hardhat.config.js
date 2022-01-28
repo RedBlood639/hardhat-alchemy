1380d389b09009724c709caba966270289b74179
redblood639 <albinorabbithunter@gmail.com>
2022-01-28 16:54:47 +0800

    feature: Dapp frontend

--- hardhat-simple/hardhat.config.js
+++ hardhat-simple/hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 const ALCHEMY_API_KEY = "FlOf6LAzxykDJEYcn-F2qlgkyxHu8JGv";
 
 const ROPSTEN_PRIVATE_KEY =
-  "5c05cc731fd7123d907d7d1468ac7f7ba37251f6d6e46c0c11457111d98f59fe";
+  "7be6355ca247aa87f11825365ba75c77b2e69e59645cc1cb981eed3b934ed0f8";
 
 module.exports = {
   solidity: "0.7.3",
