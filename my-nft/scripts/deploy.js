2ea0c522a33e15cdb0579c07c04d27a6ead10ab6
Redblood639 <albinorabbithunter@gmail.com>
2022-03-18 04:45:46 +0800

    feature : HOW TO MINT AN NFT

---
+++ my-nft/scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  await myNFT.deployed();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+//   0xC57BcF6E5969BA70760CDf07181D671d5A3EC7Cb
